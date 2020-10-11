76e00403823da100f1e75d54d8d66f2dddf15ce9
Pedro Costa de Albuquerque Lins <pedrolins835@gmail.com>
2020-10-10 23:08:46 -0300

    Código funcional
    Um update um pouco brusco, lógica geral e banco de palavras pronto

--- Source.py
+++ Source.py
@@ -1,26 +1,63 @@
-
-ArquivoFeliz = open(r"PalavrasRelacionadasFELICIDADE.txt","r")
+#Abre os documentos de texto para análise.
+ArquivoFeliz   = open(r"PalavrasRelacionadasFELICIDADE.txt","r")
 ArquivoRaivoso = open(r"PalavrasRelacionadasRAIVA.txt","r")
-ArquivoTriste = open(r"PalavrasRelacionadasTRSITEZA.txt","r")
+ArquivoTriste  = open(r"PalavrasRelacionadasTRISTEZA.txt","r")
 
+#Transforma cada documento de texto em uma lista de palavras.
 def listador(TXT):
     Listagem = []
     for linha in TXT:
-        Listagem.append(linha)
+        Listagem.append(linha.rstrip())
     return Listagem
 
-ListaFeliz = listador(ArquivoFeliz)
+#Cada grupo de palavras separado na lista adequada.
+ListaFeliz   = listador(ArquivoFeliz)
 ListaRaivosa = listador(ArquivoRaivoso)
-ListaTriste = listador(ArquivoTriste)
+ListaTriste  = listador(ArquivoTriste)
 
-Feliz = 0
-Triste = 0
-Raivoso = 0
-
+#Input inicial a ser comparado.
 texto = input("Escreva uma frase: ")
 
-listinha = texto.split()
+#Tranforma a frase numa lista.
+Listinha = texto.split()
 
-print(listinha)
+def veredito():
+    #Acumuladores para contabilizar a aparição de palavras que representem certo sentimento.
+    PtsFelizes  = 0
+    PtsRaivosos = 0
+    PtsTristes  = 0
+    Encontrado  = False
+    #Uma sequencia de comparações para preencher os acumuladores
+    for palavra in Listinha:
+        Encontrado = False
+        for comparada in ListaFeliz:
+            if palavra.casefold() == comparada.casefold():
+                PtsFelizes += 1
+                Encontrado = True
+                break
+        if Encontrado:
+            break
+        for comparada in ListaRaivosa:
+            if palavra.casefold() == comparada.casefold():
+                PtsRaivosos += 1
+                Encontrado = True
+                break
+        if Encontrado:
+            break
+        for comparada in ListaTriste:
+            if palavra.casefold() == comparada.casefold():
+                PtsTristes += 1
+                break
+    #Uma avaliação das pontuações e a declaração da maior        
+    if PtsFelizes > PtsRaivosos and PtsFelizes > PtsTristes:
+        return "Felicidade"
+    elif PtsRaivosos > PtsFelizes and PtsRaivosos > PtsTristes:
+        return "Raiva"
+    elif PtsTristes  > PtsFelizes and PtsTristes > PtsRaivosos:
+        return "Tristeza"
+    else:
+        return "Inconclusivo"
+
+print(veredito())
 
 print("-Finalizado-")
---
+++ TesteLivre.py
@@ -0,0 +1,6 @@
+#Lugar para teste de código sem compromisso.
+
+texto = input("txt: ")
+Lista = texto.split()
+
+print(Lista)
