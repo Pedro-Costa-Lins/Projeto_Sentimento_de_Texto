f59236b4a4d8341ac6c3e98e478e3baf357fa3ed
Pedro Costa de Albuquerque Lins <pedrolins835@gmail.com>
2020-10-11 15:02:54 -0300

    Refinamento
    Negação, vírgulas, aperfeiçoamento do algorítimo.

--- Source.py
+++ Source.py
@@ -1,26 +1,35 @@
-#Abre os documentos de texto para análise.
+#--==<Abre os documentos de texto para análise.>==--
 ArquivoFeliz   = open(r"PalavrasRelacionadasFELICIDADE.txt","r")
 ArquivoRaivoso = open(r"PalavrasRelacionadasRAIVA.txt","r")
 ArquivoTriste  = open(r"PalavrasRelacionadasTRISTEZA.txt","r")
 
-#Transforma cada documento de texto em uma lista de palavras.
+#Transforma cada documento de texto em uma lista de palavras.>==--
 def listador(txt):
     Listagem = []
     for linha in txt:
         Listagem.append(linha.rstrip())
     return Listagem
 
-#Cada grupo de palavras separado na lista adequada.
+#--==<Tranforma a frase numa lista, remove vírgulas de palavras que terminam com as mesmas e adiciona a vírgula como parte idempendente da lista.>==--
+def formatador(texto):
+    Lista = texto.split()
+    Adicionado = False
+    for i in range(len(Lista)):
+        if Adicionado:
+            Adicionado = False
+            continue
+        if Lista[i][-1] == ",":
+            Lista[i].replace(",", "")
+            Lista.insert(i + 1, ",")
+            Adicionado = True
+    return Lista
+
+#--==<Cada grupo de palavras separado na lista adequada.>==--
 ListaFeliz   = listador(ArquivoFeliz)
 ListaRaivosa = listador(ArquivoRaivoso)
 ListaTriste  = listador(ArquivoTriste)
 
-#Input inicial a ser comparado.
-texto = input("Escreva uma frase: ")
-
-#Tranforma a frase numa lista.
-Listinha = texto.split()
-
+#--==<Avalia a frase e devolve um resultado.>==--
 def veredito():
     #Acumuladores para contabilizar a aparição de palavras que representem certo sentimento.
     PtsFelizes  = 0
@@ -28,9 +37,10 @@ def veredito():
     PtsTristes  = 0
     Encontrado  = False
     Negação     = False
+    
     #Uma sequencia de comparações para preencher os acumuladores
     for palavra in Listinha:
-        #Reseta o boleano
+        #Reseta o boleano de controle.
         Encontrado = False
         
         #Se uma virgula for encontrada, a negação perde o efeito.
@@ -39,7 +49,7 @@ def veredito():
         
         #Ao ser identificada, a próxima palavra que expressar sentimento antes de uma vírgula vai ter um efeito negativo na pontuação de sentimento.
         #De imediato começa a análise da próxima palavra.
-        if palavra == "Não" or palavra == "nem":
+        if palavra.casefold() == "não" or palavra.casefold() == "nem":
             Negação = True
             continue
         
@@ -77,6 +87,9 @@ def veredito():
                     else:
                         PtsTristes += 1
                         break
+    #Mostra a pontuação
+
+
 
     #Uma avaliação das pontuações e a declaração da maior        
     if PtsFelizes > PtsRaivosos and PtsFelizes > PtsTristes:
@@ -87,6 +100,11 @@ def veredito():
         return "Tristeza"
     else:
         return "Inconclusivo"
+
+#--==<Input inicial a ser comparado.>==--
+texto = input("Escreva uma frase: ")
+
+Listinha = formatador(texto)
 
 print(veredito())
 
--- TesteLivre.py
+++ TesteLivre.py
@@ -1,6 +1,18 @@
 #Lugar para teste de código sem compromisso.
 
-texto = input("txt: ")
-Lista = texto.split()
+texto = input("Escreva uma frase: ")
 
-print(Lista)
+#Tranforma a frase numa lista, remove vírgulas de palavras que terminam com as mesmas e adiciona a vírgula como parte idempendente da lista.
+Listinha = texto.split()
+
+Adicionado = False
+for i in range(len(Listinha)):
+    if Adicionado:
+        Adicionado = False
+        continue
+    if Listinha[i][-1] == ",":
+        Listinha[i].replace(",", "")
+        Listinha.insert(i + 1, ",")
+        Adicionado = True
+
+print(Listinha)
