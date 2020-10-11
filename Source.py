3d3cb4792c986756425d30dd02c77b7339c9c3c0
Pedro Costa de Albuquerque Lins <pedrolins835@gmail.com>
2020-10-11 00:00:31 -0300

    Update Negação
    Agora o programa consegue interpretar negação de um sentimento até que uma vírgula seja inserida.

--- Source.py
+++ Source.py
@@ -4,9 +4,9 @@ ArquivoRaivoso = open(r"PalavrasRelacionadasRAIVA.txt","r")
 ArquivoTriste  = open(r"PalavrasRelacionadasTRISTEZA.txt","r")
 
 #Transforma cada documento de texto em uma lista de palavras.
-def listador(TXT):
+def listador(txt):
     Listagem = []
-    for linha in TXT:
+    for linha in txt:
         Listagem.append(linha.rstrip())
     return Listagem
 
@@ -27,25 +27,56 @@ def veredito():
     PtsRaivosos = 0
     PtsTristes  = 0
     Encontrado  = False
+    Negação     = False
     #Uma sequencia de comparações para preencher os acumuladores
     for palavra in Listinha:
+        #Reseta o boleano
         Encontrado = False
+        
+        #Se uma virgula for encontrada, a negação perde o efeito.
+        if palavra == ",":
+            Negação = False 
+        
+        #Ao ser identificada, a próxima palavra que expressar sentimento antes de uma vírgula vai ter um efeito negativo na pontuação de sentimento.
+        #De imediato começa a análise da próxima palavra.
+        if palavra == "Não" or palavra == "nem":
+            Negação = True
+            continue
+        
+        #Comparador.
         for comparada in ListaFeliz:
             if palavra.casefold() == comparada.casefold():
-                PtsFelizes += 1
-                Encontrado = True
-                break
+                if Negação:
+                    PtsFelizes -= 1
+                    Encontrado  = True
+                    Negação     = False
+                    break
+                else:
+                    PtsFelizes += 1
+                    Encontrado  = True
+                    break
         if Encontrado == False:
             for comparada in ListaRaivosa:
                 if palavra.casefold() == comparada.casefold():
-                    PtsRaivosos += 1
-                    Encontrado = True
-                    break
+                    if Negação:
+                        PtsRaivosos -= 1
+                        Encontrado   = True
+                        Negação      = False
+                        break
+                    else:
+                        PtsRaivosos += 1
+                        Encontrado   = True
+                        break
         if Encontrado == False:
             for comparada in ListaTriste:
                 if palavra.casefold() == comparada.casefold():
-                    PtsTristes += 1
-                    break
+                    if Negação:
+                        PtsTristes -= 1
+                        Negação     = False
+                        break
+                    else:
+                        PtsTristes += 1
+                        break
 
     #Uma avaliação das pontuações e a declaração da maior        
     if PtsFelizes > PtsRaivosos and PtsFelizes > PtsTristes:
